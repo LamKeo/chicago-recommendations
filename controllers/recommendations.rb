1dd619dd9a77456c4b38c3e49e7dc3034d3d01bb
Lamthong Keophalychanh <thea913@gmail.com>
2015-12-01 16:11:32 -0600

    controllers, it's workinggg

--- controllers/recommendations.rb
+++ controllers/recommendations.rb
@@ -1,12 +1,37 @@
 class RecommendationsController < ApplicationController
 
 get '/' do
+  @recommendations = Recommendation.all
+  erb :read_recommendations
+end
+
+get '/create' do
+  erb :create_recommendation
+end
+
+post '/create' do
+  p params
+  @rec = Recommendation.new
+  @rec.contributor = params[:contributor]
+  @rec.place_name = params[:name]
+  @rec.type_of_place = params[:type_of_place]
+  @rec.notes = params[:notes]
+  @rec.save
+
   erb :index
 end
+
 
 get '/api' do
   #select * from recommendations;
   Recommendation.all.to_json
-  
-  end
+end
+
+#using params object to access :id and convert it to json.
+get '/api/:id' do
+  p params
+  return Recommendation.find(params[:id]).to_json
+end
+
+
 end
